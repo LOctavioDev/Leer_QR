da85ee540e546a5ada34cf14c95731d3a4d6d17a
LOctavioDev <octaviodevtech@gmail.com>
2023-09-26 16:20:46 -0600

    Initial commit

---
+++ app/src/main/java/com/example/leer_qr/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.leer_qr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_
+    private Button btnTakeqr;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
